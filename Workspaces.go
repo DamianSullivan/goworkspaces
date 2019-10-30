d860531d737ad29efd75f0cdfe9e85822979e6ca
Damian Sullivan <djcsdjcs@gmail.com>
2019-10-30 16:54:29 -0500

    work in progress

--- Workspaces.go
+++ Workspaces.go
@@ -2,7 +2,6 @@ package workspaces
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"os/exec"
 	"strconv"
@@ -10,7 +9,10 @@ import (
 )
 
 func GetWorkspaceNames() []string {
-	cmd := exec.Command("gsettings", "get", "org.cinnamon.desktop.wm.preferences", "workspace-names")
+	cmd := exec.Command(
+		"gsettings", "get",
+		"org.cinnamon.desktop.wm.preferences",
+		"workspace-names")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
@@ -18,12 +20,29 @@ func GetWorkspaceNames() []string {
 	out_str := string(out)
 	out_str = strings.Replace(out_str, "'", "\"", -1)
 
-	var ws []string
-	json_err := json.Unmarshal([]byte(out_str), &ws)
+	var workspaces []string
+	json_err := json.Unmarshal([]byte(out_str), &workspaces)
 	if json_err != nil {
 		log.Fatalln("error:", json_err)
 	}
-	return ws
+	return workspaces
+}
+
+func GetWorkspaceName(desktop_id int) string {
+	workspaces := GetWorkspaceNames()
+	if desktop_id > len(workspaces)-1 {
+		log.Fatalln("Desktop id out of range")
+	}
+	return workspaces[desktop_id]
+}
+
+func GetCurrentWorkspaceName() string {
+	workspaces := GetWorkspaceNames()
+	desktop_id := GetCurrentDesktopId()
+	if desktop_id > len(workspaces)-1 {
+		log.Fatalln("Desktop id out of range")
+	}
+	return workspaces[desktop_id]
 }
 
 func GetCurrentDesktopId() int {
@@ -33,9 +52,12 @@ func GetCurrentDesktopId() int {
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	desktop_id, parse_error := strconv.Atoi(strings.TrimSuffix(string(out), "\n"))
+	desktop_id, parse_error := strconv.Atoi(
+		strings.TrimSuffix(string(out), "\n"))
 	if parse_error != nil {
-		log.Fatalf("Could not convert output from xdotool to int: %s %s", parse_error, string(out))
+		log.Fatalf(
+			"Could not convert output from xdotool to int: %s %s",
+			parse_error, string(out))
 	}
 	return desktop_id
 }
